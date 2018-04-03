11e56e03d9ad077c60c8fd780f97b0311dd81698
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-04-03 23:46:14 +0200

    learn event sourcing and planning add it to this app

--- CompanyCars.Core/Domain/Customers/Customer.cs
+++ CompanyCars.Core/Domain/Customers/Customer.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using CompanyCars.Core.Domain.Purchases;
+using System;
+using System.Collections.Generic;
 
 namespace CompanyCars.Core.Domain.Customers
 {
     public class Customer : BaseEntity
     {
-        public int UserId { get; set; }
+        private List<Payment> _payments = new List<Payment>();
+
+        public Guid Id { get; set; }
         public CustomerType Type { get; set; }
         public string Name { get; set; }
         public string CompanyName { get; set; }
@@ -12,6 +16,9 @@ namespace CompanyCars.Core.Domain.Customers
         public string Email { get; set; }
         public DateTime UpdatedAt { get; set; }
         public DateTime CreatedAt { get; set; }
+        public IEnumerable<Payment> Payments => _payments.AsReadOnly();
+
+        
 
         public enum CustomerType
         {
