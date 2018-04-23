78f507eef3f8ad511f04af43b30919bc07041cf8
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-04-23 22:43:21 +0200

    add autofac container

---
+++ src/PKShop.Common.IoC/Container/AutofacContainer.cs
@@ -0,0 +1,13 @@
+using Autofac;
+
+namespace PKShop.Common.IoC
+{
+    public class AutofacContainer : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            
+        }
+    }
+}
+
