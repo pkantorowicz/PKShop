41d2b287c16f99838a5f4f1d5a08fed36da76696
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-05-22 17:15:15 +0200

    try to push first event

---
+++ src/PKShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PKShop.Core.Notifications;
+using PKShop.Struct.Services.Services;
+using PKShop.Struct.Services.ViewModels;
+using System.Threading.Tasks;
+
+namespace PKShop.Web.Controllers
+{
+    public class ProductController : BaseController
+    {
+        private readonly IProductService _productservice;
+
+        public ProductController(IProductService productService, 
+            INotificationHandler<DomainNotification> notification) : base(notification)
+        {
+            _productservice = productService;
+        }
+
+        [HttpGet]
+        [Route("products/browse")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _productservice.BrowseAsync());
+        }
+
+        [HttpGet]
+        [Route("products/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("products/create")]
+        public async Task<IActionResult> Create(ProductViewModel productVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productVM);
+            }
+            await _productservice.CreateAsync(productVM);
+
+            if (IsValidOperation())
+            {
+                ViewBag.Success = "Product Created!";
+            }
+            return View(productVM);
+        }
+    }
+}
---
+++ src/PKShop.Web/Extensions/GuardForNulls.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PKShop.Web.Extensions
+{
+    public static class GuardForNulls
+    {
+        public static void CheckForNulls(this object obj, string name)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+    }
+}
