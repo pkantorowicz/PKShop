efa79b8894caadd8b61294733fe48892574b5e98
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-05-24 18:39:37 +0200

    Complete controller methods

--- src/PKShop.Web/Controllers/ProductController.cs
+++ src/PKShop.Web/Controllers/ProductController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using PKShop.Core.Notifications;
 using PKShop.Struct.Services.Services;
 using PKShop.Struct.Services.ViewModels;
+using PKShop.Web.Extensions;
+using System;
 using System.Threading.Tasks;
 
 namespace PKShop.Web.Controllers
@@ -46,6 +48,62 @@ namespace PKShop.Web.Controllers
                 ViewBag.Success = "Product Created!";
             }
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("products/edit/{id:guid}")]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            id.CheckForNulls(nameof(id));
+            var product = await _productservice.GetAsync(id);
+            product.CheckForNulls(nameof(product));
+            return View(product);
+        }
+
+        [HttpPost]
+        [Route("products/edit/{id:guid}")]
+        public async Task<IActionResult> Edit(ProductViewModel productVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productVM);
+            }
+            await _productservice.UpdateAsync(productVM);
+            if (IsValidOperation())
+            {
+                ViewBag.Success = "Product updated!";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("products/remove/{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            id.CheckForNulls(nameof(id));
+            var product = await _productservice.GetAsync(id);
+            product.CheckForNulls(nameof(product));
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [Route("products/remove/{id:guid}")]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            await _productservice.RemoveAsync(id);
+            if (IsValidOperation())
+            {
+                return View(await _productservice.GetAsync(id));
+            }
+            ViewBag.Success = "Product removed!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Route("products/history/{id:guid}")]
+        public async Task<JsonResult> History(Guid id)
+        {
+            var productFromHistory = await _productservice.GetHistoryDataAsync(id);
+            return Json(productFromHistory);
         }
     }
 }
